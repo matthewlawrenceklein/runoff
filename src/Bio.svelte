53423127b0ebddd12542668cc25e86f7fc418b7d
Matthew Klein <mtt.l.kln@gmail.com>
2021-09-13 18:35:37 +0000

    stub out bio component

---
+++ src/Bio.svelte
@@ -0,0 +1,30 @@
+<script>
+    // your script goes here
+</script>
+    <div class='container'>
+        <h3>byline goes here</h3>
+        <p> 
+            details go here and whatever else you want to type blah blah blah blah blah
+            details go here and whatever else you want to type blah blah blah blah blah
+            details go here and whatever else you want to type blah blah blah blah blah
+            details go here and whatever else you want to type blah blah blah blah blah
+            
+        </p>
+    </div>
+<style>
+    .container{
+        height: 220;
+        width: 600px;
+        background-color: #141921;
+        color: white;
+        margin-top: 50px;
+        border-radius: 15px;
+    }
+
+    h3{
+        text-align: center;
+    }
+    /* your styles go here */
+</style>
+
+<!-- markup (zero or more items) goes here -->

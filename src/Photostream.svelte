0995b47ea956139461ef918c4ba20d0254fd6197
Matthew Klein <mtt.l.kln@gmail.com>
2021-09-13 18:35:23 +0000

    sort by post date

--- src/Photostream.svelte
+++ src/Photostream.svelte
@@ -1,5 +1,6 @@
 <script>
     import PostCard from './PostCard.svelte'
+    import Bio from './Bio.svelte'
     import { getStorage, ref, listAll, getDownloadURL  } from "firebase/storage";
     import { collection, query, where, getDocs, getFirestore } from "firebase/firestore";
     import { onMount } from "svelte";
@@ -22,6 +23,7 @@
         querySnapshot.forEach((doc) => {
             photoDocs.set([...$photoDocs, doc])
         });
+        photoDocs.set($photoDocs.sort((a,b) => (a.data()['postDate'] < b.data()['postDate']) ? 1 : ((b.data()['postDate'] < a.data()['postDate']) ? -1 : 0)))
         $photoDocs.map(doc => {
             getURL(doc.data()['imgName'])
         })
@@ -37,18 +39,19 @@
     }
 
 </script>
-    {#if $imageURLs.length > 0}
-        <div class='container'>
-            {#if loading}
-                 <Circle3 size="60" color="#FF3E00" unit="px" duration='1s'></Circle3> 
-            {/if}
-            {#each [...$photoDocs] as photo}
-                 <PostCard {photo} url={$imageURLs.find(url => url.includes(photo.data()['imgName']))}}/>
-            {/each}
-        </div>
-    {:else}
-        <Circle3 size="60" color="#FF3E00" unit="px" duration='1s'></Circle3> 
-    {/if}
+    <div class='container'>
+        <Bio/>
+        {#if $imageURLs.length > 0}
+                {#if loading}
+                     <Circle3 size="60" color="#FF3E00" unit="px" duration='1s'></Circle3> 
+                {/if}
+                {#each [...$photoDocs] as photo}
+                     <PostCard {photo} url={$imageURLs.find(url => url.includes(photo.data()['imgName']))}}/>
+                {/each}
+        {:else}
+            <Circle3 size="60" color="#FF3E00" unit="px" duration='1s'></Circle3> 
+        {/if}
+    </div>
 <style>
     .container{
         display: flex;
@@ -56,6 +59,7 @@
         justify-content: center;
         align-items: center;
         border-radius: 10px;
+        width: 100vw;
     }
     /* your styles go here */
 </style>

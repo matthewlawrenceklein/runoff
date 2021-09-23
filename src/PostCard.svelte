4b40584eec082b451e8e0ef2e372ac135b1f06ff
Matthew Klein <mtt.l.kln@gmail.com>
2021-09-23 21:02:11 +0000

    spelling ;)

--- src/PostCard.svelte
+++ src/PostCard.svelte
@@ -7,7 +7,7 @@
     let showDescriptionDetails = false
     
     onMount(() => {
-        if (photo.data()['description'].length > 70) photoConcat = photo.data()['description'].slice(0,70) + '...(click to exand)' 
+        if (photo.data()['description'].length > 70) photoConcat = photo.data()['description'].slice(0,70) + '...(click to expand)' 
         date = new Date(1000 * photo.data()['postDate']).toString().slice(0,10)
     })
 

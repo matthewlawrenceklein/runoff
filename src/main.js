1141cd2c635715ec73c9a15f4e595288b242a700
Matthew Klein <mtt.l.kln@gmail.com>
2021-10-13 22:53:03 +0000

    point to user-settable config elements

--- src/main.js
+++ src/main.js
@@ -2,15 +2,9 @@ import App from './App.svelte';
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-	apiKey: "AIzaSyAsU2T8m0hK-ngmvpcJ2nQ-Z-fskjRcD0w",
-	authDomain: "rachel-photostream.firebaseapp.com",
-	projectId: "rachel-photostream",
-	storageBucket: "rachel-photostream.appspot.com",
-	messagingSenderId: "735584074089",
-	appId: "1:735584074089:web:55cf43347608eb7ef6e5f8",
-	measurementId: "G-D6DLNHHXV4"
+	// TODO: add your config data
   };
-  
+
   // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 
